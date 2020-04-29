c5c20ad3eb388ddc4c8116471aabaa7030132e87
Ngonyoku <47557555+Ngonyoku@users.noreply.github.com>
2020-04-30 01:22:34 +0400

    Users and Members Classes Implimented.

--- Classes/Auth.php
+++ Classes/Auth.php
@@ -2,11 +2,6 @@
 
 class Auth extends User
 {
-    private $_dbh,
-        $_data,
-        $_sessionName,
-        $_cookieName,
-        $_isLoggedIn;
 
     public function __construct($user = null)
     {
@@ -26,35 +21,6 @@ class Auth extends User
             }
         } else {
             $this->find($user);
-        }
-    }
-
-    //This method checks if the user is existent in the database
-    public function find($user = null)
-    {
-        #If parameter is a number then we Check Id column Otherwise we Check the username column in Database
-        if ($user) {
-            $field = (is_numeric($user)) ? 'user_id' : 'member_number';
-            $data = $this->_dbh->selectAll('users', array($field, '=', $user));
-
-            if ($data->count()) {
-                $this->_data = $data->first(); #the $_data variable holds the first record returned from the database.
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    //The methods is used to update User Details.
-    public function update($fields = array(), $user_id = null)
-    {
-        if (!$user_id && $this->isLoggedIn()) {
-            $user_id = $this->data()->user_id;
-        }
-
-        if (!$this->_dbh->update('users', $user_id, $fields)) {
-            throw new Exception("Failed To Update Details");
         }
     }
 
@@ -146,21 +112,9 @@ class Auth extends User
         return false;
     }
 
-    //it returns the data stored in the database.
-    public function data()
-    {
-        return $this->_data;
-    }
-
     //The method checks if there is any data which has been returned from the database.
     public function exists()
     {
         return (!empty($this->data())) ? true : false;
-    }
-
-    //Returns the current User state(i.e if he/she is Logged In or Not)
-    public function isLoggedIn()
-    {
-        return $this->_isLoggedIn;
     }
 }
